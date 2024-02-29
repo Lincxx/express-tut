ea3bbc301026550c16e2603658f63624499852d2
lincxx <bossc@BOSSCO>
2024-02-29 06:55:26 -0500

    Added in paths and static assets

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express')
+//we need to use the absolute path to get a file, so we will use a builtin package
+const path = require('path')
 
 //now we invoke express and save it to a var
 const app = express()
 
+//setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+})
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
+})
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+    console.log('server listening on port 5000...')
 })
 
 // methods we are going to use the most
 // app.get
 // app.post
 // app.delete
-// app.all
+// app.all - works with all of the above
 // app.use - used for middleware
 // app.listen
