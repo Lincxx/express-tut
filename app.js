a5e10bd41b0918d0c1c9d9098293bd87563d2998
lincxx <bossc@BOSSCO>
2024-03-01 08:42:44 -0500

    Middleware - Setup

--- app.js
+++ app.js
@@ -1,55 +1,25 @@
 const express = require('express')
 const app = express()
 
-const {products} = require('./data')
+//middleware is everywhere in express apps. One could argue, that express apps are nothing but middleware
+//pattern
+// req => middleware => res
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+const logger  = (req, res, next) => {
+    const method = req.method;
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    next()
+}
+
+app.get('/', logger, (req, res) => {
+
+    res.send('Home')
 })
 
-app.get('/api/products', (req, res) => {
-    const newProduct = products.map((product) => {
-        const {id, name, image} = product
-        return {id, name, image}
-    })
-    res.json(newProduct)
-})
-
-app.get('/api/products/:productId', (req, res) => {
-    const {productId} = req.params
-    const singleProduct = products.find((product) => product.id === Number(productId))
-    if(!singleProduct) {
-        return res.status(404).send('Product does not exist!')
-    }
-    res.json(singleProduct)
-})
-
-app.get('/api/products/:productId/reviews/:reviewId', (req,res) => {
-    console.log(req.params)
-    res.send('jello')
-})
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query)
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if(search) {
-        sortedProducts = sortedProducts.filter((product)=> {
-            return product.name.startsWith(search)
-        })
-    }
-
-    if(limit) {
-        sortedProducts = sortedProducts.slice(0,Number(limit))
-    }
-
-    if(sortedProducts.length < 1) {
-        //res.status(200).send("no product matched")
-        return res.status(200).json({success:true, data:[]})
-    }
-
-    return res.status(200).json(sortedProducts)
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
 app.listen(5000, () => {
---
+++ oldapp.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+
+const {products} = require('./data')
+
+//middleware is everywhere in express apps. One could argue, that express apps are nothing but middleware
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProduct = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(newProduct)
+})
+
+app.get('/api/products/:productId', (req, res) => {
+    const {productId} = req.params
+    const singleProduct = products.find((product) => product.id === Number(productId))
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist!')
+    }
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productId/reviews/:reviewId', (req,res) => {
+    console.log(req.params)
+    res.send('jello')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product)=> {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+
+    if(sortedProducts.length < 1) {
+        //res.status(200).send("no product matched")
+        return res.status(200).json({success:true, data:[]})
+    }
+
+    return res.status(200).json(sortedProducts)
+})
+
+app.listen(5000, () => {
+    console.log('Server up')
+})
